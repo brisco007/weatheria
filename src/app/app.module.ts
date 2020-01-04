cc3f5f3e8263055f9f205db283a2338d891a3e2e
AVOMO Aurel Steve <avomoa@gmail.com>
2020-01-04 22:37:25 +0100

    bd

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,37 @@ import { WeatherListComponent } from "./weather-list/weather-list.component";
 import { WeatherElementComponent } from "./weather-element/weather-element.component";
 import { UserProfileComponent } from "./user-profile/user-profile.component";
 
+//database configuratio
+const dbConfig = {
+  name: 'Weatheria_database',
+  version: 1,
+  objectStoresMeta:  [
+    {
+      store: 'user',
+      storeConfig: {keyPath: 'id', autoIncrement: true},
+      storeSchema: [
+        {username: 'username', keyPath: 'username', options: {unique: true}},
+        {password: 'password', keyPath: 'password', options: {unique: false}},
+        {name: 'name', keyPath: 'name', options: {unique: false}},
+        {bird_date: 'bird_date', keyPath: 'bird_date', options: {unique: false}},
+        {proile: "profile", keyPath: 'profile', options: {unique: false}}
+      ]
+    },
+    {
+      store: 'address',
+      storeConfig: {keyPath: 'id', autoIncrement: true},
+      storeSchema: [
+        {city: 'city', keyPath: 'city', options: {unique: true}},
+        {state: 'state', keyPath: 'state', options: {unique: false}},
+        {country: 'country', keyPath: 'country', options: {unique: false}},
+        {pos_X: 'pos_X', keyPath: 'pos_X', options: {unique: false}},
+        {pos_Y: "pos_Y", keyPath: 'pos_Y', options: {unique: false}}
+      ]
+    }
+  ]
+};
+
+
 @NgModule({
   declarations: [
     AppComponent,
---
+++ src/app/services/database.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import { NgxIndexedDBModule } from 'ngx-indexed-db';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DatabaseService {
+  
+
+  constructor() {
+
+   }
+}
