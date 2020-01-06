d109159e535a1d422bef0650fe80275dc961047b
prisca-makso <prisca.faveur.makso@gmail.com>
2020-01-06 11:29:18 +0100

    branche-prisca

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { MapComponent } from './map/map.component';
+import { CarteComponent } from './carte/carte.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MapComponent,
+    CarteComponent
   ],
   imports: [
     BrowserModule,
