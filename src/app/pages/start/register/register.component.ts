194da7d6cb8f23c9e55652c1b06233fae3a3afef
Brisco <dteguia396@gmail.com>
2020-01-09 04:41:23 +0100

    merge avec la page de log et register2

--- src/app/pages/start/register/register.component.ts
+++ src/app/pages/start/register/register.component.ts
@@ -1,46 +1,50 @@
-import { FormGroup, Validators } from '@angular/forms';
-import { Component, OnInit } from '@angular/core';
+import { FormGroup, Validators } from "@angular/forms";
+import { Component, OnInit } from "@angular/core";
+import { DatabaseService } from "src/app/services/database.service";
 
 @Component({
-  selector: 'app-register',
-  templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  selector: "app-register",
+  templateUrl: "./register.component.html",
+  styleUrls: ["./register.component.css"]
 })
 export class RegisterComponent implements OnInit {
   [x: string]: any;
 
   registerForm: FormGroup;
-  constructor(private databaseService: DatabaseService) { }
+  constructor(private databaseService: DatabaseService) {}
 
   ngOnInit() {
     this.initForm();
   }
 
-  initForm(){
+  initForm() {
     this.registerForm = this.formBuilder.group({
       username: ["", Validators.required],
-      name: ['', Validators.required],
+      name: ["", Validators.required],
       password: ["", Validators.required],
       confirm_password: ["", Validators.required],
-      bird_date: ['', Validators.required],
+      bird_date: ["", Validators.required]
     });
   }
 
-  onSubmit(){
-    if(!this.registerForm.invalid){
+  onSubmit() {
+    if (!this.registerForm.invalid) {
       const formValue = this.registerForm.value;
       let user = {
-        username: formValue['username'],
-        password: formValue['password'],
-        name: formValue['name'],
-        bird_date: formValue['bird_date']
-      }
-      databaseService.register(user, () => {
-        //todo
-      }, ()=> {
-        //todo
-      });
+        username: formValue["username"],
+        password: formValue["password"],
+        name: formValue["name"],
+        bird_date: formValue["bird_date"]
+      };
+      this.databaseService.register(
+        user,
+        () => {
+          //todo
+        },
+        () => {
+          //todo
+        }
+      );
     }
   }
-
 }
